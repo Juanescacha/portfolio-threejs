7a7717993940af62eb22f1e7f80bdd373c621545
Juan Esteban Camargo <juanescacha@hotmail.com>
2024-10-15 20:42:54 -0500

    feat: 🎨 add Hero component to App and refactor main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./sections/Navbar.jsx"
+import Hero from "./sections/Hero.jsx"
 
 const App = () => {
 	return (
-		<main className="max-w-7xl mx-auto">
+		<main className="mx-auto max-w-7xl">
 			<Navbar />
+			<Hero />
 		</main>
 	)
 }
